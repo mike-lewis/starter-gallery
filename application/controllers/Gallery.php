9b20c7992999e75a0c4adbd4eed7cfb89c88c634
Mike Lewis <mike_lewis_@hotmail.com>
2016-01-19 13:26:12 -0800

    second section

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gallery extends CI_Controller {
+class Gallery extends Application {
 
     /**
      * Index Page for this controller.
